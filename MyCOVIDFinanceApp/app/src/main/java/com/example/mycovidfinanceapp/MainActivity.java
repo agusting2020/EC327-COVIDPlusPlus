583d24c2b68ef056ac2983cdf467743a229210b7
agusting2020 <agusting@bu.edu>
2020-04-28 03:53:54 -0500

    updated home fragment

--- MyCOVIDFinanceApp/app/src/main/java/com/example/mycovidfinanceapp/MainActivity.java
+++ MyCOVIDFinanceApp/app/src/main/java/com/example/mycovidfinanceapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.mycovidfinanceapp;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -11,15 +14,43 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.PieChartData;
+import lecho.lib.hellocharts.model.SliceValue;
 import lecho.lib.hellocharts.view.PieChartView;
 
 public class MainActivity extends AppCompatActivity {
 
+    PieChartView pieChartView_monthly;
+//    ListView listView;
+
+//    String homeSummary[] = {"Checking", "Savings", "Credit"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        ListView listView = findViewById(R.id.HomeSummary);
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.listview, R.id.textView, homeSummary);
+//        listView.setAdapter(adapter);
+
+//        pieChartView_monthly = findViewById(R.id.piechart_monthly);
+//
+//        List pieData;
+//        pieData = new ArrayList<>();
+//
+//        pieData.add(new SliceValue(15, Color.BLUE).setLabel("Q1: $10"));
+//        pieData.add(new SliceValue(25, Color.GRAY).setLabel("Q2: $4"));
+//        pieData.add(new SliceValue(10, Color.RED).setLabel("Q3: $18"));
+//        pieData.add(new SliceValue(60, Color.MAGENTA).setLabel("Q4: $28"));
+//
+//        PieChartData pieChartData = new PieChartData(pieData);
+//        pieChartData.setHasLabels(true).setValueLabelTextSize(14);
+//        pieChartData.setHasCenterCircle(true).setCenterText1("Sales in million").setCenterText1FontSize(20).setCenterText1Color(Color.parseColor("#0097A7"));
+//        pieChartView_monthly.setPieChartData(pieChartData);
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
