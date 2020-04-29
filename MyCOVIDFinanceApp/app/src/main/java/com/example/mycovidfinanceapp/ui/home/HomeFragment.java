d0b8bd03d57c875a3ba00a578309e363943cd1a7
agusting2020 <agusting@bu.edu>
2020-04-28 21:19:48 -0500

    updated pie chart

--- MyCOVIDFinanceApp/app/src/main/java/com/example/mycovidfinanceapp/ui/home/HomeFragment.java
+++ MyCOVIDFinanceApp/app/src/main/java/com/example/mycovidfinanceapp/ui/home/HomeFragment.java
@@ -1,48 +1,88 @@
 package com.example.mycovidfinanceapp.ui.home;
 
-import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.mycovidfinanceapp.R;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
-public class HomeFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
 
-//    public Context mContext;
-//
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        mContext=context;
-//    }
+public class HomeFragment extends Fragment implements OnChartValueSelectedListener {
 
     private HomeViewModel homeViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
-        final TextView textView = root.findViewById(R.id.text_month);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        ;
+        PieDataSet dataSet;
+        PieData pieData;
+
+
+        List<PieEntry> Values = new ArrayList<>();
+        Values.add(new PieEntry(8f, 0));
+        Values.add(new PieEntry(15f, 1));
+        Values.add(new PieEntry(12f, 2));
+        Values.add(new PieEntry(25f, 3));
+
+        List<String> Category = new ArrayList<>();
+        Category.add("Category 1");
+        Category.add("Category 2");
+        Category.add("Category 3");
+        Category.add("Category 4");
+
+        dataSet = new PieDataSet(Values, "Categorical Spending");
+
+        pieData = new PieData(dataSet);
+
+        PieChart pieChart = root.findViewById(R.id.piechart);
+
+        pieChart.setData(pieData);
+
+        pieChart.getDescription().setText("");
+
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setTransparentCircleRadius(25f);
+        pieChart.setHoleRadius(25f);
+
+        dataSet.setColors(ColorTemplate.VORDIPLOM_COLORS);
+        pieData.setValueTextSize(13f);
+        pieData.setValueTextColor(Color.BLACK);
+        pieChart.setOnChartValueSelectedListener(this);
+
+        pieChart.animateXY(1400, 1400);
 
         return root;
+    }
+
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+        if (e == null)
+            return;
+        Log.i("VAL SELECTED", "Value: " + e.getY() + ", xIndex: " + e.getX());
+    }
+
+
+    @Override
+    public void onNothingSelected() {
+        Log.i("PieChart", "nothing selected");
     }
 }
